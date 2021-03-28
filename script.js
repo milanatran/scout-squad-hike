6b76e8c722dc65d69482be43ced6946df671b9e2
Milana Tran <milanatran@Milanas-MBP.fritz.box>
2021-03-28 20:05:48 +0200

    Added all boxes with quizzes, tipps, images and answers

--- script.js
+++ script.js
@@ -40,101 +40,53 @@ function answerB1R2() {
     }
 }
 
-//TIPP LSG BOX 1 RÄTSEL 3
-
-function tip1b1r3() {
-    alert("Hier ist Tipp 1!");
-}
-
-function tip2b1r3() {
-    alert("Hier ist Tipp 2!");
-}
-
-function tip3b1r3() {
-    alert("Hier ist Tipp 3!");
-}
-
-function answerB1R3() {
-    let input = document.getElementById("inputB1R3").value;
-    var answer = "hello"; //hier dann die richtige Lösung
-    if (input == answer) {
-        alert("Richtig!");
-    } else {
-        alert("Falsch!");
-    }
-}
-
 //BOX 2
 
 //TIPP LSG BOX 2 RÄTSEL 1
 
 function tip1b2r1() {
-    alert("Hier ist Tipp 1!");
+    alert("Das Wort, was wir suchen, hat 6 Buchstaben.");
 }
 
 function tip2b2r1() {
-    alert("Hier ist Tipp 2!");
+    alert("Das Lösungswort ist ein Feiertag.");
 }
 
 function tip3b2r1() {
-    alert("Hier ist Tipp 3!");
+    alert("An dem Feiertag gibt es viele Eier.");
 }
 
 function answerB2R1() {
-    let input = document.getElementById("inputB2R1").value;
-    var answer = "hello"; //hier dann die richtige Lösung
+    let input = document.getElementById("inputB2R1").value.toLowerCase();
+    var answer = "ostern"; //hier dann die richtige Lösung
     if (input == answer) {
-        alert("Richtig!");
+        alert("Das ist korrekt!");
     } else {
-        alert("Falsch!");
+        alert("Versuch es nochmal.");
     }
 }
 
 //TIPP LSG BOX 2 RÄTSEL 2
 
 function tip1b2r2() {
-    alert("Hier ist Tipp 1!");
+    alert("Die Punkte haben die gleiche Funktion wie die Punkte auf den Karten in großen Einkaufscentern.");
 }
 
 function tip2b2r2() {
-    alert("Hier ist Tipp 2!");
+    alert("Die Buchstaben unter den Kreisen sind die Himmelsrichtungen.");
 }
 
 function tip3b2r2() {
-    alert("Hier ist Tipp 3!");
+    alert("Der erste Kreis ergibt die Zahl 1.");
 }
 
 function answerB2R2() {
     let input = document.getElementById("inputB2R2").value;
-    var answer = "hello"; //hier dann die richtige Lösung
+    var answer = 1536; //hier dann die richtige Lösung
     if (input == answer) {
-        alert("Richtig!");
+        alert("Korrekt! Weiter so.");
     } else {
-        alert("Falsch!");
-    }
-}
-
-//TIPP LSG BOX 2 RÄTSEL 3
-
-function tip1b2r3() {
-    alert("Hier ist Tipp 1!");
-}
-
-function tip2b2r3() {
-    alert("Hier ist Tipp 2!");
-}
-
-function tip3b2r3() {
-    alert("Hier ist Tipp 3!");
-}
-
-function answerB2R3() {
-    let input = document.getElementById("inputB2R3").value;
-    var answer = "hello"; //hier dann die richtige Lösung
-    if (input == answer) {
-        alert("Richtig!");
-    } else {
-        alert("Falsch!");
+        alert("Leider falsch...");
     }
 }
 
@@ -143,71 +95,85 @@ function answerB2R3() {
 //TIPP LSG BOX 3 RÄTSEL 1
 
 function tip1b3r1() {
-    alert("Hier ist Tipp 1!");
+    alert("Verbinde die Formen.");
 }
 
 function tip2b3r1() {
-    alert("Hier ist Tipp 2!");
+    alert("Die Reihenfolge zum Verbinden steht in der Geschichte.");
 }
 
 function tip3b3r1() {
-    alert("Hier ist Tipp 3!");
+    alert("Die erste Ziffer ist 2.");
 }
 
 function answerB3R1() {
     let input = document.getElementById("inputB3R1").value;
-    var answer = "hello"; //hier dann die richtige Lösung
+    var answer = 243; //hier dann die richtige Lösung
     if (input == answer) {
-        alert("Richtig!");
+        alert("Das ist die richtige Antwort!");
     } else {
-        alert("Falsch!");
+        alert("Das ist die falsche Antwort...");
     }
 }
 
-//TIPP LSG BOX 2 RÄTSEL 2
-
-function tip1b3r2() {
-    alert("Hier ist Tipp 1!");
-}
-
-function tip2b3r2() {
-    alert("Hier ist Tipp 2!");
-}
-
-function tip3b3r2() {
-    alert("Hier ist Tipp 3!");
-}
+//LSG BOX 3 RÄTSEL 2
 
 function answerB3R2() {
-    let input = document.getElementById("inputB3R2").value;
-    var answer = "hello"; //hier dann die richtige Lösung
+    let input = document.getElementById("inputB3R2").value.toLowerCase;
+    var answer = "b"; //hier dann die richtige Lösung
     if (input == answer) {
         alert("Richtig!");
     } else {
-        alert("Falsch!");
+        alert("Leider nicht korrekt.");
     }
 }
 
-//TIPP LSG BOX 2 RÄTSEL 3
+//BOX 4
 
-function tip1b3r3() {
+//TIPP LSG BOX 4 RÄTSEL 1
+
+function tip1b4r1() {
+    alert("Schau dir die fett markierten Wörter an und vergleiche die mit den Büchern. Was fällt dir auf?");
+}
+
+function tip2b4r1() {
+    alert("Zu jedem Buch gehört eine Nummer.");
+}
+
+function tip3b4r1() {
+    alert("Die Reihenfolge, in der die Wörter in der Geschichte auftauchen, ist wichtig.");
+}
+
+function answerB4R1() {
+    let input = document.getElementById("inputB4R1").value;
+    var answer = 6739; //hier dann die richtige Lösung
+    if (input == answer) {
+        alert("Bravo! Richtige Antwort.");
+    } else {
+        alert("Oh nein, du hast dich vertan!");
+    }
+}
+
+//TIPP LSG BOX 4 RÄTSEL 2
+
+function tip1b4r2() {
     alert("Hier ist Tipp 1!");
 }
 
-function tip2b3r3() {
+function tip2b4r2() {
     alert("Hier ist Tipp 2!");
 }
 
-function tip3b3r3() {
+function tip3b4r2() {
     alert("Hier ist Tipp 3!");
 }
 
-function answerB3R3() {
-    let input = document.getElementById("inputB3R3").value;
-    var answer = "hello"; //hier dann die richtige Lösung
+function answerB4R2() {
+    let input = document.getElementById("inputB4R2").value;
+    var answer = 35; //hier dann die richtige Lösung
     if (input == answer) {
-        alert("Richtig!");
+        alert("Wunderbar! Das war korrekt.");
     } else {
-        alert("Falsch!");
+        alert("Das war nicht die richtige Antwort.");
     }
 }
